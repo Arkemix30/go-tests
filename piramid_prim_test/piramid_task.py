cb57b23a1b96f7afc2f853076493008cd7d00aff
Arkemix30 <silvacompte@gmail.com>
2022-09-12 19:49:27 -0600

    ref(piramid_prim): refactoring code for using full recursion

--- piramid_prim_test/piramid_task.py
+++ piramid_prim_test/piramid_task.py
@@ -1,10 +1,15 @@
-def piramid_prim(n: int):
-    # If n is lower or equal to 0, then we return 0
+def piramid_prim(n: int, pointer:int = 0, output: str = ""):
+    # If N is lower or equal to 0, then we return 0
     if n <= 0:
         return 0
-    # For loop iteration for printing purposes
-    data = [ str(i) for i in range(n,0,-1)]
-    # Join array for output printing as string
-    print("".join(data))
-    # Calling function for recursion
-    return piramid_prim(n-1)
+    # If output string is empty, we set current N value to pointer
+    if output == "":
+        pointer=n
+    # If pointer is equal to 0, then we print the curren output
+    # and we decrease N value for next N iteration
+    if pointer <= 0:
+        print(output)
+        return piramid_prim(n-1, n-1, output="")
+    # Saving pointer value to outpout string and decreasing pointer value for iteration
+    output += str(pointer)
+    return piramid_prim(n, pointer-1, output)
